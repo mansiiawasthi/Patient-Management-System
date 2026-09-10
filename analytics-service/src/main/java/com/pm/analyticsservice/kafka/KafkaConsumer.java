8843e0ae8f994c5bef42093470e6bcb151daa2a3
mansiiawasthi <mansiawasthi55@gmail.com>
2026-09-11 00:32:53 +0530

    issue resolved-analytics-service

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -16,9 +16,14 @@ public class KafkaConsumer {
     public void consumeEvent(byte[] event){
         try {
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
-            log.info("Received Patient Event: {}", patientEvent);
+            // perform any  business related analytics here
+            log.info("Received Patient Event:  [PatientId={}, PatientName={},PatientEmail={}]",
+                    patientEvent.getPatientId(),
+                    patientEvent.getName(),
+                    patientEvent.getEmail());
+
         } catch (InvalidProtocolBufferException e) {
-            log.error("Error deserializing patient event", e);
+            log.error("Error deserializing patient event", e.getMessage());
         }
     }
 }
